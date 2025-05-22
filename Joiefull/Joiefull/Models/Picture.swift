cb0d65c5ac1ab8d643ad38113fdc65593fd974a5
Elodie Desmoulin <elodie.dsmln@icloud.com>
2025-05-22 20:21:04 +0200

    Create Models : Article & Picture and moving .json inside xcode project

---
+++ Joiefull/Joiefull/Models/Picture.swift
@@ -0,0 +1,14 @@
+//
+//  Picture.swift
+//  Joiefull
+//
+//  Created by Elo on 22/05/2025.
+//
+
+
+import Foundation
+
+struct Picture: Codable {
+    let url: String
+    let description: String
+}
